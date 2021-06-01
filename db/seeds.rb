c57a7b16eeed898a411c978dfd425666b774c735
Raul Palacios <palacios.raul02@gmail.com>
2021-05-31 22:51:45 -0500

    Add simple seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+3.times do
+	Season.create_with(
+		name: 'Dragon Ball',
+		description: 'Dragon Ball es un manga escrito e ilustrado por Akira Toriyama. Fue publicado originalmente en la revista Shōnen Jump, de la editorial japonesa Shūeisha, entre 1984 y 1995',
+		episodes: 250
+	)
+end
